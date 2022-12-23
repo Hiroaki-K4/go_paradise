fa4c88c2b28270ec7d496ce8368bc29e36d526b0
Hiroaki-K4 <49no19@gmail.com>
2022-12-23 10:19:06 +0900

    Update appendix

--- appendix/ex_01/main.go
+++ appendix/ex_01/main.go
@@ -1,14 +1,24 @@
 package main
 
-import {
+import (
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 
 	"github.com/rs/zerolog/log"
-}
+)
 
 func main() {
-	http.HandleFunc("/hello", func())
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World")
+		log.Info().Msg("receive hello world request")
+	})
+	fmt.Println("Start listening at :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "")
+		io.WriteString(os.Stderr, err.Error())
+		os.Exit(1)
+	}
 }
