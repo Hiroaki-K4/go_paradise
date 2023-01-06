77213669c345e58bc7012d875b2baef8c7d47ddc
Hiroaki-K4 <49no19@gmail.com>
2023-01-06 17:19:12 +0900

    Add streaming process

--- app/cmd/client/main.go
+++ app/cmd/client/main.go
@@ -6,8 +6,13 @@ import (
 	"os"
 	"log"
 	"context"
+	"errors"
+	"io"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/status"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/credentials/insecure"
+	_ "google.golang.org/genproto/googleapis/rpc/errdetails"
 	hellopb "mygrpc/pkg/grpc"
 )
 
@@ -16,14 +21,205 @@ var (
 	client hellopb.GreetingServiceClient
 )
 
+func Hello() {
+	fmt.Println("Please enter your name.")
+	scanner.Scan()
+	name := scanner.Text()
+
+	req := &hellopb.HelloRequest{
+		Name: name,
+	}
+	ctx := context.Background()
+	md := metadata.New(map[string]string{"type": "unary", "from": "client"})
+	ctx = metadata.NewOutgoingContext(ctx, md)
+	// res, err := client.Hello(context.Background(), req)
+	var header, trailer metadata.MD
+	// res, err := client.Hello(ctx, req)
+	res, err := client.Hello(ctx, req, grpc.Header(&header), grpc.Trailer(&trailer))
+	if err != nil {
+		// fmt.Println(err)
+		if stat, ok := status.FromError(err); ok {
+			fmt.Printf("code: %s\n", stat.Code())
+			fmt.Printf("message: %s\n", stat.Message())
+			fmt.Printf("details: %s\n", stat.Details())
+		} else {
+			fmt.Println(err)
+		}
+	} else {
+		fmt.Println(header)
+		fmt.Println(trailer)
+		fmt.Println(res.GetMessage())
+	}
+}
+
+func HelloServerStream() {
+	fmt.Println("Please enter your name.")
+	scanner.Scan()
+	name := scanner.Text()
+
+	req := &hellopb.HelloRequest{
+		Name: name,
+	}
+	stream, err := client.HelloServerStream(context.Background(), req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		res, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Println("all th responses have already received.")
+			break
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(res)
+	}
+}
+
+func HelloClientStream() {
+	stream, err := client.HelloClientStream(context.Background())
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	sendCount := 5
+	fmt.Printf("Please enter %d names.\n", sendCount)
+	for i := 0; i < sendCount; i++ {
+		scanner.Scan()
+		name := scanner.Text()
+
+		if err := stream.Send(&hellopb.HelloRequest{
+			Name: name,
+		}); err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	res, err := stream.CloseAndRecv()
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(res.GetMessage())
+	}
+}
+
+func HelloBiStreams() {
+	ctx := context.Background()
+	md := metadata.New(map[string]string{"type": "stream", "from": "client"})
+	ctx = metadata.NewOutgoingContext(ctx, md)
+	// stream, err := client.HelloBiStreams(context.Background())
+	stream, err := client.HelloBiStreams(ctx)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	sendNum := 5
+	fmt.Printf("Please enter %d names.\n", sendNum)
+
+	var sendEnd, recvEnd bool
+	sendCount := 0
+	for !(sendEnd && recvEnd) {
+		if !sendEnd {
+			scanner.Scan()
+			name := scanner.Text()
+
+			sendCount++
+			if err := stream.Send(&hellopb.HelloRequest{
+				Name: name,
+			}); err != nil {
+				fmt.Println(err)
+				sendEnd = true
+			}
+
+			if sendCount == sendNum {
+				sendEnd = true
+				if err := stream.CloseSend(); err != nil {
+					fmt.Println(err)
+				}
+			}
+		}
+
+		var headerMD metadata.MD
+		if !recvEnd {
+			if headerMD == nil {
+				headerMD, err = stream.Header()
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println(headerMD)
+				}
+			}
+
+			if res, err := stream.Recv(); err != nil {
+				if !errors.Is(err, io.EOF) {
+					fmt.Println(err)
+				}
+				recvEnd = true
+			} else {
+				fmt.Println(res.GetMessage())
+			}
+		}
+	}
+
+	trailerMD := stream.Trailer()
+	fmt.Println(trailerMD)
+}
+
+func myUnaryClientInteceptor1(ctx context.Context, method string, req, res interface{}, cc *grpc.ClientConn, invoker grpc.UnaryInvoker, opts ...grpc.CallOption) error {
+	fmt.Println("[pre] my unary client interceptor 1", method, req)
+	err := invoker(ctx, method, req, res, cc, opts...)
+	fmt.Println("[post] my unary client interceptor 1", res)
+	return err
+}
+
+func myStreamClientInteceptor1(ctx context.Context, desc *grpc.StreamDesc, cc *grpc.ClientConn, method string, streamer grpc.Streamer, opts ...grpc.CallOption) (grpc.ClientStream, error) {
+	log.Println("[pre] my stream client interceptor 1", method)
+
+	stream, err := streamer(ctx, desc, cc, method, opts...)
+	return &myClientStreamWrapper1{stream}, err
+}
+
+type myClientStreamWrapper1 struct {
+	grpc.ClientStream
+}
+
+func (s *myClientStreamWrapper1) SendMsg(m interface{}) error {
+	log.Println("[pre message] my stream client interceptor 1: ", m)
+	return s.ClientStream.SendMsg(m)
+}
+
+func (s *myClientStreamWrapper1) RecvMsg(m interface{}) error {
+	err := s.ClientStream.RecvMsg(m)
+
+	if !errors.Is(err, io.EOF) {
+		log.Println("[post message] my stream client interceptor 1: ", m)
+	}
+	return err
+}
+
+func (s *myClientStreamWrapper1) CloseSend() error {
+	err := s.ClientStream.CloseSend()
+
+	log.Println("[post] my stream client interceptor 1")
+	return err
+}
+
 func main() {
 	fmt.Println("Start gRPC client.")
 
 	scanner = bufio.NewScanner(os.Stdin)
 
-	address := "localhost:8080"
+	address := "localhost:50051"
 	conn, err := grpc.Dial(
 		address,
+		grpc.WithStreamInterceptor(myStreamClientInteceptor1),
 		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithBlock(),
 	)
@@ -38,7 +234,10 @@ func main() {
 
 	for {
 		fmt.Println("1: send Request")
-		fmt.Println("2: exit")
+		fmt.Println("2: HelloServerStream")
+		fmt.Println("3: HelloClientStream")
+		fmt.Println("4: HelloBiStream")
+		fmt.Println("5: exit")
 		fmt.Print("please enter >")
 
 		scanner.Scan()
@@ -48,26 +247,16 @@ func main() {
 		case "1":
 			Hello()
 		case "2":
+			HelloServerStream()
+		case "3":
+			HelloClientStream()
+		case "4":
+			HelloBiStreams()
+		case "5":
 			fmt.Println("bye.")
 			goto M
 		}
 	}
 M:
 
-}
-
-func Hello() {
-	fmt.Println("Please enter your name.")
-	scanner.Scan()
-	name := scanner.Text()
-
-	req := &hellopb.HelloRequest{
-		Name: name,
-	}
-	res, err := client.Hello(context.Background(), req)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println(res.GetMessage())
-	}
 }
--- app/cmd/server/main.go
+++ app/cmd/server/main.go
@@ -7,9 +7,15 @@ import (
 	"context"
 	"net"
 	"time"
+	"errors"
+	"io"
 	"os/signal"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/metadata"
 	"google.golang.org/grpc/reflection"
+	// "google.golang.org/grpc/codes"
+	// "google.golang.org/grpc/status"
+	// "google.golang.org/genproto/googleapis/rpc/errdetails"
 	hellopb "mygrpc/pkg/grpc"
 )
 
@@ -21,10 +27,83 @@ func NewMyServer() *myServer {
 	return &myServer{}
 }
 
+func myUnaryServerInterceptor1(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	log.Println("[pre] my unary server interceptor 1: ", info.FullMethod)
+	res, err := handler(ctx, req)
+	log.Println("[post] my unary server interceptor 1: ", res)
+	return res, err
+}
+
+func myUnaryServerInterceptor2(ctx context.Context, req interface{}, info *grpc.UnaryServerInfo, handler grpc.UnaryHandler) (interface{}, error) {
+	log.Println("[pre] my unary server interceptor 2: ", info.FullMethod, req)
+	res, err := handler(ctx, req)
+	log.Println("[post] my unary server interceptor 2: ", res)
+	return res, err
+}
+
+func myStreamServerInterceptor1(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	log.Println("[pre stream] my stream server interceptor 1: ", info.FullMethod)
+
+	err := handler(srv, &myServerStreamWrapper1{ss})
+
+	log.Println("[post stream] my stream server interceptor 1: ")
+	return err
+}
+
+func myStreamServerInterceptor2(srv interface{}, ss grpc.ServerStream, info *grpc.StreamServerInfo, handler grpc.StreamHandler) error {
+	log.Println("[pre stream] my stream server interceptor 2: ", info.FullMethod)
+
+	err := handler(srv, &myServerStreamWrapper1{ss})
+
+	log.Println("[post stream] my stream server interceptor 2: ")
+	return err
+}
+
+type myServerStreamWrapper1 struct {
+	grpc.ServerStream
+}
+
+func (s *myServerStreamWrapper1) RecvMsg(m interface{}) error {
+	err := s.ServerStream.RecvMsg(m)
+	if !errors.Is(err, io.EOF) {
+		log.Println("[pre message] my stream server interceptor 1: ", m)
+	}
+	return err
+}
+
+func (s *myServerStreamWrapper1) SendMsg(m interface{}) error {
+	log.Println("[post message] my stream server interceptor 1: ", m)
+	return s.ServerStream.SendMsg(m)
+}
+
 func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	// err := status.Error(codes.Unknown, "unknown error occured")
+
+	if md, ok := metadata.FromIncomingContext(ctx); ok {
+		log.Println(md)
+	}
+
+	headerMD := metadata.New(map[string]string{"type": "unary", "from": "server", "in": "header"})
+	if err := grpc.SetHeader(ctx, headerMD); err != nil {
+		return nil, err
+	}
+
+	trailerMD := metadata.New(map[string]string{"type": "unary", "from": "server", "in": "trailer"})
+	if err := grpc.SetTrailer(ctx, trailerMD); err != nil {
+		return nil, err
+	}
+
 	return &hellopb.HelloResponse{
 		Message: fmt.Sprintf("Hello, %s!", req.GetName()),
 	}, nil
+
+	// stat := status.New(codes.Unknown, "unknown error occurred")
+	// // stat, _ = stat.WithDetails()
+	// stat, _ = stat.WithDetails(&errdetails.DebugInfo{
+	// 	Detail: "detail reason of err",
+	// })
+	// err := stat.Err()
+	// return nil, err
 }
 
 func (s *myServer) HelloServerStream(req *hellopb.HelloRequest, stream hellopb.GreetingService_HelloServerStreamServer) error {
@@ -40,14 +119,69 @@ func (s *myServer) HelloServerStream(req *hellopb.HelloRequest, stream hellopb.G
 	return nil
 }
 
+func (s *myServer) HelloClientStream(stream hellopb.GreetingService_HelloClientStreamServer) error {
+	nameList := make([]string, 0)
+	for {
+		req, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			message := fmt.Sprintf("Hello, %v!", nameList)
+			return stream.SendAndClose(&hellopb.HelloResponse{
+				Message: message,
+			})
+		}
+		if err != nil {
+			return err
+		}
+		nameList = append(nameList, req.GetName())
+	}
+}
+
+func (s *myServer) HelloBiStreams(stream hellopb.GreetingService_HelloBiStreamsServer) error {
+	if md, ok := metadata.FromIncomingContext(stream.Context()); ok {
+		log.Println(md)
+	}
+
+	headerMD := metadata.New(map[string]string{"type": "stream", "from": "server", "in": "header"})
+	if err := stream.SetHeader(headerMD); err != nil {
+		return err
+	}
+
+	trailerMD := metadata.New(map[string]string{"type": "stream", "from": "server", "in": "trailer"})
+	stream.SetTrailer(trailerMD)
+
+	for {
+		req, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			return nil
+		}
+		if err != nil {
+			return err
+		}
+		message := fmt.Sprintf("Hello, %v!", req.GetName())
+		if err := stream.Send(&hellopb.HelloResponse{
+			Message: message,
+		}); err != nil {
+			return err
+		}
+	}
+}
+
 func main() {
-	port := 8080
+	port := 50051
 	listner, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		panic(err)
 	}
 
-	s := grpc.NewServer()
+	// s := grpc.NewServer(
+	// 	grpc.UnaryInterceptor(myUnaryServerInterceptor1),
+	// )
+	s := grpc.NewServer(
+		grpc.ChainStreamInterceptor(
+			myStreamServerInterceptor1,
+			myStreamServerInterceptor2,
+		),
+	)
 
 	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
 
